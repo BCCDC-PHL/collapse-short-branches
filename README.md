58ef66a5c39447928cca74ce0291347db409f1cc
Dan Fornika <dfornika@gmail.com>
2020-11-24 09:51:21 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# collapse-short-branches
+
+This script assists in the identification of genomic clusters in newick-format phylogenetic trees using [TreeCluster](https://github.com/niemasd/TreeCluster)
+
+It performs the following transformations on the tree:
+
+1. Collapse all branches shorter than `--minimum_branch_length` (default 0.000001) down to zero.
+2. Re-order the nodes of the tree by:
+  1. Number of descendants
+  2. Edge length
+  3. Label
+  ...in descending order
+3. Resolve polytomies
+
+## Usage
+
+```
+collapse_short_branches.py <tree.nwk> [--min_branch_length BRANCH_LENGTH]
+```
+
+The collapsed tree will be printed to standard output. It can be redirected into a file using the output redirection `>` operator.
---
+++ collapse_short_branches.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+import treeswift
+
+def main(args):
+    t = treeswift.read_tree(args.tree, 'newick')
+    t.collapse_short_branches(args.min_branch_length)
+
+    # Leaves aren't shortened by collapse_short_branch_lengths()
+    # so shorten them manually by iterating over all leaves
+    for node in t.traverse_leaves():
+        if node.get_edge_length() <= args.min_branch_length:
+            node.set_edge_length(0)
+
+    # If we use the default ascending=True, this breaks TreeCluster clustering
+    t.order("num_descendants_then_edge_length_then_label", ascending=False)
+    t.resolve_polytomies()
+    print(t)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tree')
+    parser.add_argument('--min_branch_length', type=float, default=0.000001)
+    args = parser.parse_args()
+    main(args)
