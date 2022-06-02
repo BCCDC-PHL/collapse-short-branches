8110909e69eb77ba762ae8490f5b8ac20a8453fa
Dan Fornika <dfornika@gmail.com>
2022-06-02 09:19:25 -0700

    Add scale factor

--- collapse_short_branches.py
+++ collapse_short_branches.py
@@ -16,11 +16,17 @@ def main(args):
     # If we use the default ascending=True, this breaks TreeCluster clustering
     t.order("num_descendants_then_edge_length_then_label", ascending=False)
     t.resolve_polytomies()
+
+    if args.scale:
+        t.scale_edges(args.scale)
+
     print(t)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('tree')
     parser.add_argument('--min_branch_length', type=float, default=0.000001)
+    parser.add_argument('--scale', type=float)
     args = parser.parse_args()
     main(args)
